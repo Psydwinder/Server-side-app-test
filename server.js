284587bd65f7ab34e9fd237f1511f27923518c79
Niete <nieteis@gmail.com>
2023-02-05 11:52:36 +0000

    HTTP Post request with fetch

--- server.js
+++ server.js
@@ -3,5 +3,17 @@ const express = require('express');
 const server = express();
 
 server.use(express.static('public'));
+server.use(express.json({limit: '1mb'}));
+
+server.post('/',(request,response)=>{
+    console.log('I got a request!');
+    console.log(request.body);
+    const data = request.body;
+    response.json({
+        status: 'success',
+        latitude: data.lat,
+        longitude: data.long,
+    })
+})
 
 module.exports = server;
